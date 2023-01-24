c4d3702cca21d1664263973fa75cef9c99a57aab
AnoNe17e <atomx361@gmail.com>
2023-01-24 18:53:14 +0700

    layouts admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return "asd";
+// });
+
+// Route::get('/', [DashboardController::class, 'index'])->name('admin.index');
+Route::get('/', [DashboardController::class, 'index']);
+
+
+Route::prefix('admin')->group(function () {
 });
